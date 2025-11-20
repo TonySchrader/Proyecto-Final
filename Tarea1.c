767db2a6af0c76236e111b56200ef32362f7a298
TonySchrader <schraderantonio@gmail.com>
2025-11-19 21:15:24 -0600

    Update.c

---
+++ Tarea1.c
@@ -0,0 +1,50 @@
+/*Schrader Rodriguez Luis Antonio
+Proyecto Final*/
+
+#include <stdio.h>
+void leerVotos();
+
+int voto1[100], voto2[100], voto3[100];
+int totalOyentes;
+
+void main()
+{
+    leerVotos();
+
+    int i;
+    printf("\nResumen de votos:\n");
+    for (i = 0; i < totalOyentes; i++)
+    {
+        printf("Oyente %d: %d %d %d\n", i, voto1[i], voto2[i], voto3[i]);
+    }
+}
+
+/*tarea 1*/
+
+    void leerVotos()
+    {
+        int c1, c2, c3; 
+        i = 0;
+
+        printf("Concurso de radio\n");
+        printf("Introduce 3 canciones por oyente (escoje del 0 al 9). Para que terminar teclea -1.\n\n");
+
+        while (i < 100)
+        {
+            printf("Oyente %d (3 canciones): ", i);
+            scanf("%d", &c1);
+
+            if (c1 == -1)
+            break;
+
+            scanf("%d", &c2);
+            scanf("%d", &c3);
+
+            voto1[i] = c1;
+            voto2[i] = c2;
+            voto3[i] = c3;
+            i++;
+        }
+
+    totalOyentes = i;
+    } 
